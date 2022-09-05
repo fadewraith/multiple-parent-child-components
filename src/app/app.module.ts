61abb2a15fd73bf29eb19a0fc8036142018a78d3
fade-wraith <doodledante22@gmail.com>
2022-09-05 22:23:23 +0530

    multiple parent child components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { Child1Component } from './components/child1/child1.component';
+import { Child2Component } from './components/child2/child2.component';
+import { Child3Component } from './components/child3/child3.component';
+import { Child4Component } from './components/child4/child4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    Child1Component,
+    Child2Component,
+    Child3Component,
+    Child4Component
   ],
   imports: [
     BrowserModule,
